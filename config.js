f64e09e21e9d3f029f5110bf8df04a01c03b1b76
caderitter <ritter.cade@gmail.com>
2020-01-10 22:52:55 -0600

    add holyshit

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ module.exports.commandToFileMap = {
   'who wants coffee?': 'who-wants-coffee-but-read-the-description-headphone-warning.mp3',
   'imperialmarch': 'Star Wars- The Imperial March (Darth Vader\'s Theme).mp3',
   'starwars': 'Star Wars Main Theme (Full).mp3',
+  'holyshit': 'savetweetvid_SeBmGcUqWUusck7F.mp3'
 };
