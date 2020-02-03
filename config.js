e557ad42b548750c46addc24e862e74f03c3b05a
Cade Ritter <ritter.cade@gmail.com>
2020-02-03 11:17:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const airtableConfig = {
-  BASE_ID: 'appcLczgHk2kiv2Pc',
-  TABLE_ID: 'yell-bot',
-  MESSAGE_COLUMN_ID: 'Command',
-  FILE_COLUMN_ID: 'Audio file to play',
-  FORM_URL: 'https://airtable.com/shrq3sefXjlth5MJ2'
+  BASE_ID: '',
+  TABLE_ID: '',
+  MESSAGE_COLUMN_ID: '',
+  FILE_COLUMN_ID: '',
+  FORM_URL: ''
 };
 
 module.exports = { airtableConfig };
